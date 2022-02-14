b8631d7ef593e865229681ad6ed7bc2ce58b26a0
cqbzljh <799839896@qq.com>
2022-02-14 15:13:47 +0800

    222

--- music-server/src/main/java/com/example/yin/constant/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.yin.constant;
-
-public class Constants {
-    // MAC、Linux系统
-    public static final String RESOURCE_MAC_PATH = "/Users/hongweiyin/Documents/github-workspace/music-website/music-server";
-
-    // windos系统
-    public static final String RESOURCE_WIN_PATH = "C:\\Users\\hongweiyin\\Documents\\github-workspace\\music-website\\music-server";
-}
--- music-server/src/main/java/com/example/yin/service/AdminService.java
+++
@@ -1,6 +0,0 @@
-package com.example.yin.service;
-
-public interface AdminService {
-
-    boolean veritypasswd(String name, String password);
-}
